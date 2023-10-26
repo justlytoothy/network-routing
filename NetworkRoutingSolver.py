69805c32c11eb25aef73564769deb0d92bf7a202
Jake <jake@schillingcentral.net>
2023-10-26 17:51:58 -0600

    d

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -7,143 +7,113 @@ import time
 
 class ArrayPriorityQueue:
     def __init__(self, nodes):
-        self.queue = []
-        for node in nodes:
-            self.insert(node, sys.maxsize)
-
-    def insert(self, node, key):
-        self.queue.append((node, key))
-
-    def delete_min(self):
-        if not self.queue:
-            return None
-        min_val = sys.maxsize
-        min_node = None
-        min_index = 0
-        for i in range(len(self.queue)):
-            node = self.queue[i]
-            if min_val > node[1]:
-                min_val = node[1]
-                min_node = node[0]
-                min_index = i
-
-        return self.queue.pop(min_index)
-
-    def decrease_key(self, node, new_key):
-        for i in range(len(self.queue)):
-            if self.queue[i][0] == node:
-                if new_key < self.queue[i][1]:
-                    self.queue[i] = (node, new_key)
-                break
-
-
-class PriorityArray:
-    def __init__(self, nodes):
-        self.arr = []
         self.dictionary = {}
         for node in nodes:
             self.insert(node, sys.maxsize)
 
+    # O(1)
     def insert(self, node, key):
-        self.arr.append((node, key))
-        self.dictionary[node] = len(self.arr) - 1
+        self.dictionary[node.node_id] = (node, key)  # O(1) to get length and assign new map entry
 
-    def get_distance(self, node):
-        index = self.dictionary[node]
-        return self.arr[index][1]
-
+    # O(|V|)
     def delete_min(self):
-        minVal = sys.maxsize
-        minNode = None
-        for node in self.arr:
-            if node[1] < minVal:
-                minVal = node[1]
-                minNode = node[0]
-        index = self.dictionary[minNode]
-        return self.arr.pop(index)
+        if not self.dictionary:
+            return None
+        min_val = sys.maxsize
+        min_index = -1
+        for node in self.dictionary.items():  # for loop runs at worst once for every vertex in graph O(|V|)
+            if min_val > node[1][1]:
+                min_val = node[1][1]
+                min_index = node[0]
+        if min_index == -1:
+            return None
+        return self.dictionary.pop(min_index)[0]        # O(1) for popping at index from dictionary
 
+    # O(1)
     def decrease_key(self, node, new_key):
-        index = self.dictionary[node]
-        self.arr[index] = (node, new_key)
-
-    def is_empty(self):
-        return len(self.arr) == 0
+        self.dictionary[node.node_id] = (node, new_key)  # O(1) to assign value at index
 
 
-class Heap:
+class BinaryHeap:
     def __init__(self, nodes):
         self.heap = []
         self.dictionary = {}
         for node in nodes:
             self.insert(node, sys.maxsize)
 
+    # O(log(|V|)
     def insert(self, node, key):
-        self.dictionary[node] = len(self.heap)
-        self.heap.append((node, key))
-        self.bubble_up(len(self.heap) - 1)
+        self.heap.append((node, key))  # O(1) to append
+        index = len(self.heap) - 1
+        self.dictionary[node] = index  # O(1) to set value in map
 
-    def get_distance(self, node):
-        index = self.dictionary[node]
-        return self.heap[index][1]
+        self.bubble_up(index)  # O(log(|V|) for bubble_up
 
+    # O(log(|V|)
     def delete_min(self):
-        if not self.heap:
+        if not self.heap:               # O(1) to check if heap not empty
             return None
 
         min_node, min_key = self.heap[0]
-        last_node, last_key = self.heap.pop()
-        del self.dictionary[min_node]
 
+        last_node, last_key = self.heap.pop()           # O(1) to pop from end
         if self.heap:
-            self.heap[0] = (last_node, last_key)
+            self.heap[0] = (last_node, last_key)        # O(1) to assign to front
             self.dictionary[last_node] = 0
-            self.bubble_down(0)
+        else:
+            del self.dictionary[last_node]              # O(1) to delete
 
-        # return min_node, min_key
+        self.trickle_down(0)                            # O(log(|V|)) for trickle_down
+
         return min_node
 
+    # O(log(|V|)
     def decrease_key(self, node, new_key):
-        if node in self.dictionary:
-            index = self.dictionary[node]
-            current_key = self.heap[index][1]
-            if new_key < current_key:
-                self.heap[index] = (node, new_key)
-                self.bubble_up(index)
+        if node not in self.dictionary:         # O(1) to check for node in dictionary
+            return
 
+        index = self.dictionary[node]           # O(1) Search in dictionary
+        current_key = self.heap[index][1]       # O(1) access list at index
+
+        if new_key < current_key:
+            self.heap[index] = (node, new_key)  # O(1) assign value
+            self.bubble_up(index)               # O(log(|V|)
+
+    # O(log(|V|)
     def bubble_up(self, index):
-        while index > 0:
-            parent = (index - 1) // 2
-            if self.heap[index][1] < self.heap[parent][1]:
-                self._swap(index, parent)
-                index = parent
+        while index > 0:        # O(log|V|) because index gets roughly halved every iteration and starts as |V|
+            parent_index = (index - 1) // 2
+            if self.heap[index][1] < self.heap[parent_index][1]:
+                index = self.swap(index, parent_index)      # O(1) to swap
             else:
                 break
 
-    def bubble_down(self, index):
-        while True:
-            left_child = 2 * index + 1
-            right_child = 2 * index + 2
+    # O(log(|V|)
+    def trickle_down(self, index):
+        while True:                                                    # O(log(|V|) worst case loop once per level of tree, max tree height is log(|V|), meaning O(log(|V|)
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
             smallest = index
 
-            if left_child < len(self.heap) and self.heap[left_child][1] < self.heap[smallest][1]:
-                smallest = left_child
+            if (left_child_index < len(self.heap) and                       # O(1) for whole if block
+                    self.heap[left_child_index][1] < self.heap[smallest][1]):
+                smallest = left_child_index
 
-            if right_child < len(self.heap) and self.heap[right_child][1] < self.heap[smallest][1]:
-                smallest = right_child
+            if (right_child_index < len(self.heap) and                       # O(1) for whole if block
+                    self.heap[right_child_index][1] < self.heap[smallest][1]):
+                smallest = right_child_index
 
             if smallest != index:
-                self._swap(index, smallest)
-                index = smallest
+                index = self.swap(index, smallest)                  # O(1) to swap
             else:
                 break
 
-    def _swap(self, index, parent):
-        self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
-        self.dictionary[self.heap[index][0]] = index
-        self.dictionary[self.heap[parent][0]] = parent
-
-    def is_empty(self):
-        return len(self.heap) == 0
+    # O(1)
+    def swap(self, child, parent):
+        self.heap[child], self.heap[parent] = self.heap[parent], self.heap[child]
+        self.dictionary[self.heap[child][0]] = child            # O(1) access and modify map entry and list entry
+        self.dictionary[self.heap[parent][0]] = parent          # O(1) access and modify map entry and list entry
+        return parent
 
 
 class NetworkRoutingSolver:
@@ -164,11 +134,13 @@ class NetworkRoutingSolver:
             curr = queue.delete_min()
             if curr is None:
                 break
-            for edge in curr[0].neighbors:
-                distance = self.distances[curr[0]] + edge.length
+            # if type(curr) is tuple:
+            #     curr = curr[0]
+            for edge in curr.neighbors:
+                distance = self.distances[curr] + edge.length
                 if distance < self.distances[edge.dest]:
                     self.distances[edge.dest] = distance
-                    self.previous[edge.dest] = (curr[0], edge.length)
+                    self.previous[edge.dest] = (curr, edge.length)
                     queue.decrease_key(edge.dest, distance)
 
     def getShortestPath(self, destIndex):
@@ -178,11 +150,8 @@ class NetworkRoutingSolver:
         #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL
         #       NEED TO USE
         path_edges = []
-        total_length = 0
         node = self.network.nodes[destIndex]
-        print(self.previous[node])
         curr = node
-        edges_left = 3
         while self.previous[curr] is not None:
             edge = self.previous[curr]
             path_edges.append((curr.loc, edge[0].loc, '{:.0f}'.format(edge[1])))
@@ -197,7 +166,7 @@ class NetworkRoutingSolver:
         #       CALL TO getShortestPath(dest_index)
         queue = None
         if use_heap:
-            queue = Heap(self.network.nodes)
+            queue = BinaryHeap(self.network.nodes)
         else:
             queue = ArrayPriorityQueue(self.network.nodes)
         queue.decrease_key(self.network.nodes[srcIndex], 0)
